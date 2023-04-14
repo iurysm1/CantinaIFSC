0f6172e934aff9e9fad1921294bee756121f2fe7
Iury Silveira Marques <iurysmarques0@gmail.com>
2023-04-14 18:11:33 -0300

    teste

---
+++ CantinaIFSC/CantinaIFSC/src/model/bo/Funcionario.java
@@ -0,0 +1,14 @@
+
+package model.bo;
+
+public class Funcionario extends Pessoa{
+    
+    private String cpf;
+    private String rg;
+    private String usuario;
+    private String senha;
+
+    public Funcionario() {
+    }
+    
+}
